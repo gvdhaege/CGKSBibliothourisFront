f4152b2ba69da7f7cba9baa2709f395458247948
seppeg <seppe.gielen@cegeka.com>
2017-11-02 16:43:03 +0100

    seppeg: forgot transactional

--- src/main/java/be/cegeka/bibliothouris/application/UserController.java
+++ src/main/java/be/cegeka/bibliothouris/application/UserController.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/user")
+@Transactional
 public class UserController {
 
     @Inject
