51440a13767c3d3442bdfca2b429939ebfd62823
Gerri Vanderhaegen <gerri.vanderhaegen@cegeka.com>
2017-11-28 11:33:45 +0100

    story 6 from frontend to backend, no security atm

---
+++ bibliothouris-ui/src/app/book/AddBook.class.ts
@@ -0,0 +1,20 @@
+export class AddBook {
+    
+  title: string;
+  authorFirstName: string;
+  authorLastName: string;
+  isbn: string;
+
+      constructor(
+        title: string,
+        authorFirstName: string,
+        authorLastName: string,
+        isbn: string
+      ) {
+        this.title = title;
+        this.authorFirstName = authorFirstName;
+        this.authorLastName = authorLastName;
+        this.isbn = isbn;
+       }
+    
+    }
